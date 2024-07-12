ba325fd8278df48739fa78686c41fc1a1d444eb5
Chaliyaporn Suttisak <chaliyaporn.sts@gmail.com>
2024-07-12 17:02:01 +0700

    Edit: Routes App

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Nav from './component/Nav'
 import './index.css'
 import Artist from './component/Artist'
 import Profile from './component/Profile'
-// import Name from './component/Name'
-import Library from './component/Library'
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Artist />} />
         <Route path="/profile/:id" element={<Profile />} />
-        {/* <Route path="/profile/:id/name" element={<Name />} /> */}
-        <Route path="/profile/:id/library" element={<Library />} />
       </Routes>
     </>
   )
